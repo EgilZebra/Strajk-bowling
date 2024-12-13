d94c08ac01b9c8b5d448c6dfd69a7eb42be3ecf0
Egil Ramsten <egil.ramsten@hotmail.com>
2024-12-13 14:19:34 +0100

    update v36

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,17 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
 import Booking from './views/Booking';
 import Confirmation from './views/Confirmation';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route >
-      <Route path='/' element={<Booking />} />
-      <Route path='/confirmation' element={<Confirmation />} />
-    </Route>
-), {  basename: '/Strajk-bowling/'});
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Booking />,
+    },
+    {
+      path: '/confirmation',
+      element: <Confirmation />,
+    }
+]);
 
 export default router;
